730d1b91b4eaab4509fbfea98b0857b7d0432a8a
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2022-07-26 18:55:55 +0100

    Update cards.js

--- js/cards.js
+++ js/cards.js
@@ -1,20 +1,38 @@
 const CARDS = {
     f1_1: [
         "Nothing",
-        x=>`Does absolutely nothing`,
-        x=>true,
+        x=>`Kills your favorite anime characters`,
+        x=>data.round<10,
         x=>{},
     ],
     f1_2: [
         "Nothinger",
-        x=>`Does absolutely nothing`,
-        x=>true,
+        x=>`It's too late to run...`,
+        x=>data.round>=10&&data.round<30,
         x=>{},
     ],
     f1_3: [
         "Nothingest",
-        x=>`Does absolutely nothing`,
-        x=>true,
+        x=>`CONSOLE: IP successfully sent to Democratic Republic of Korea`,
+        x=>data.round>=30,
         x=>{},
+    ],
+    f2_1: [
+        "Energy",
+        x=>`Increase max energy by <span class="green">+2</span>`,
+        x=>data[x].maxEnergy<20,
+        x=>{data[x].maxEnergy+=2},
+    ],
+    f2_2: [
+        "Energy",
+        x=>`Increase max energy by <span class="green">+3</span>`,
+        x=>data[x].maxEnergy>=20&&data[x].maxEnergy<50,
+        x=>{data[x].maxEnergy+=3},
+    ],
+    f2_3: [
+        "Energy",
+        x=>`Increase max energy by <span class="green">+5</span>`,
+        x=>data[x].maxEnergy>=50,
+        x=>{data[x].maxEnergy+=5},
     ],
 }
