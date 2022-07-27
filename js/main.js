d373435a94d3e508b3e927a6f932e71fc2a0516d
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2022-07-27 12:08:10 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -163,10 +163,9 @@ function generateRandomCards() {
     }
 
     for (let i = 0; i < 3; i++) {
+        let [p,e] =  [ac[0][Math.floor(Math.random()*ac[0].length)],ac[1][Math.floor(Math.random()*ac[1].length)]]
         if (round != 5) {
-            let [p,e] = [ac[0][Math.floor(Math.random()*ac[0].length)],ac[1][Math.floor(Math.random()*ac[1].length)]]
-        } else {
-            let [p,e] = [CARDS.filter(x=>x[2]==false)[Math.floor(Math.random()*CARDS.filter(x=>x[2]==false).length)],CARDS.filter(x=>x[2]==false)[Math.floor(Math.random()*CARDS.filter(x=>x[2]==false).length)]]
+            [p,e] = [CARDS.filter(x=>x[2]==false)[Math.floor(Math.random()*CARDS.filter(x=>x[2]==false).length)],CARDS.filter(x=>x[2]==false)[Math.floor(Math.random()*CARDS.filter(x=>x[2]==false).length)]]
         }
         let [cp,ce] = [CARDS[p],CARDS[e]]
 
