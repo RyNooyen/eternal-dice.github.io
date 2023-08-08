1ba9506e8d5eead9914108eb2a0034fc2f376883
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2023-08-08 21:03:52 +0100

    info hub

--- js/conclusion.js
+++ js/conclusion.js
@@ -14,26 +14,19 @@ const POPUP = {
     <button onclick="removePopup();pass()">YES</button><button onclick="removePopup()">NO</button>
     `,
     scrambler: _ =>`
-    Congratulations! You made it to round 10!<br>
-    As a reward, you have now unlocked the scrambler dice!<br>
-    Every turn it will change all the dice next to it, allowing for more possibilities!<br>
+    You unlocked the <b class="blue">Scrambler Dice</b>!<br>
     <button onclick="removePopup()">Okay!</button>
     `,
     poison: _ =>`
-    Well done on making it to round 20!<br>
-    However, the enemy will start automatically gaining side progress!<br>
-    To combat this, you have the new poison dice!<br>
-    Poison dice can apply a poison effect to the enemy, and causes them to get damage dealt to them every time they use dice!<br>
-    This will come in useful as the enemy AI will not use dice if the next usage would kill them from the poison.<br>
+    You unlocked the <b class="purple">Poison Dice</b>!<br>
     <button onclick="removePopup()">Okay!</button>
     `,
-    tutorial0: _=>`
-    Do you want to see the tutorial? (There's <b>ALOT</b> of new things)<br>
-    <button onclick="setPopup(POPUP.tutorial1())">Of course!</button><button onclick="removePopup();start()">Nah.</button>
+    infohub: _=>`
+    Welcome to the info hub!<br><br>
+    <button onclick="setPopup(POPUP.infobasic())">Basic</button> <button onclick="setPopup(POPUP.infodice())">Dices</button><br>
+    <button onclick="removePopup()">Exit</button>
     `,
-    tutorial1: _=>`
-    <h2>Tutorial</h2>
-
+    infobasic: _=>`
     This game isn't for beginners. Please play <a href='https://mrredshark77.github.io/Endless-Dice/'>RedShark's version</a> first.<br><br>
     All the cards have been changed, the total amount of cards currently is ${CARDS.length}.<br>
     At round 5, you will choose between the <span style="color: yellow">major</span> cards, you will only get to choose these once and they will impact your gameplay dramatically!<br><br>
@@ -41,7 +34,16 @@ const POPUP = {
     For every 75 sides, the amount of progress to get a side increments.<br>
     For example, if you have 30 of a side, one progress is needed and if you have 75 of a side, two progress is needed.<br><br>
     You will unlock new dice as you go along, helping you fight against the enemy.<br><br>
-    <button onclick="removePopup();start()">Okay!</button>
+    <button onclick="setPopup(POPUP.infohub())">Go Back</button>
+    `,
+    infodice: _=>`
+    <b class="red">Damage Dice</b> - Deals damage to the enemy.<br>
+    <b class="orange">Swap Dice</b> - Swaps all your dice with the enemy's dice. You have a 1 in 250 chance of getting this.<br>
+    <b class="green">Heal Dice</b> - Gives yourself health.<br>
+    <b class="blue">Scrambler Dice</b> - Changes all the dice around it. Unlocked at Round 10.<br>
+    <b class="purple">Poison Dice</b> - Spreads across damage over dice usage. Unlocked at Round 20.<br>
+    <br>
+    <button onclick="setPopup(POPUP.infohub())">Go Back</button>
     `,
 }
 
